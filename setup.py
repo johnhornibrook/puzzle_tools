d0ff858c59319e4474d88b97d6262b4d941ff746
John Hornibrook <john.hornibrook@sydney.edu.au>
2025-01-14 15:39:13 +1100

    setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	Name='puzzle_tools',
+	Description='Collection of analysis tools for puzzle hunts',
+	Url='https://github.com/johnhornibrook/puzzle_tools',
+	Author='John Hornibrook',
+	Packages=['frequency_analysis'],
+	Install_requires=[]
+)
