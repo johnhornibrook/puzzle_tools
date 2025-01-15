7539aa46163b660160be5b6da9745e14016fbcbb
John Hornibrook <john.hornibrook@sydney.edu.au>
2025-01-15 23:21:52 +1100

    Handling missing letters

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # puzzle_tools
-Collection of analysis tools for puzzle hunts
+
+Puzzle Tools is a collection of Python-based analysis tools for puzzle hunts, crosswords, riddles, etc.
+
+Here is some of the functionality.
+
+## Frequency Analysis
+
+Uses frequency analysis of quadgrams (four-letter groups) to compute a likelihood that a given string is english rather than gibberish.
+
+This can be used to loop over large numbers of possibilities and examine only the most promising.
+
+It is broadly robust against erroneous letters and missing letters.
+
--- frequency_analysis/frequency_analysis.py
+++ frequency_analysis/frequency_analysis.py
@@ -66,5 +66,95 @@ class FrequencyAnalysis:
         f = filter(str.isalpha, s)
         s = "".join(f)
         return s.lower()
+    
+    """
+    Methods below handle the case where some letters are missing.
+
+    TODO: combine with the above, since these methods usually encompass the functionality above anyway
+    """
+
+    def _allowed_char_missing(self, c):
+        return c.isalpha() or c == '?'
+
+    def _clean_string_missing(self, s):
+        f = filter(self._allowed_char_missing, s)
+        s = "".join(f)
+        return s.lower()
+
+    def _score_gram_missing(self, quad):
+        # For now, handle the different numbers of '?' separately
+        unknown_count = quad.count('?')
+
+        if unknown_count == 0:
+            return self._score_gram(quad)
+        
+        # For some number of '?', sum the occurances 
+        elif unknown_count == 1:
+            n = 0
+            for i in range(26):
+                c = chr(ord('a') + i)
+                test_gram = quad.replace('?', c)
+                n += self.quadgrams[test_gram] if test_gram in self.quadgrams else self.non_existent_gram_score
+            return math.log(n / self.total_quadgram_count)
+        
+        elif unknown_count == 2:
+            n = 0
+            for i in range(26):
+                for j in range(26):
+                    bigram = chr(ord('a') + i) + chr(ord('a') + j)
+                    test_gram = self._substitute_string(quad, bigram)
+                    n += self.quadgrams[test_gram] if test_gram in self.quadgrams else self.non_existent_gram_score
+            return math.log(n / self.total_quadgram_count)
+        
+        elif unknown_count == 3:
+            # TODO: For this we can use a single-digit score. We want a standalone 'E' to be taken more
+            # seriously than a standalone 'Q'
+
+            return 0
+
+        else:
+            return 0
+    
+    def score_string_missing(self, s):
+
+        s = self._clean_string_missing(s)
+
+        score = 0
+        for i in range(len(s) - 3):
+            quadgram = s[i:i+4]
+            score += self._score_gram_missing(quadgram)
+
+        return round(score / (len(s) - 3), 3)
+    
+    def _substitute_string(self, s1, s2, replace_char='?'):
+        """
+        Replaces instances of a special character (c) in string s1 by the contents of string s2
+        """
+
+        chars_added = 0
+        new_string = ''
+
+        for c in s1:
+            if c == replace_char:
+                new_string += s2[chars_added]
+                chars_added += 1
+            else:
+                new_string += c
+        
+        return new_string
+    
+    def rank_strings_missing(self, s_array, num_to_output=10):
+        """
+        Ranks a list of strings by how likely they are to be english, and prints out the top few
+        """
+        
+        string_score_pairs = []
+        for s in s_array:
+            string_score_pairs.append((s, self.score_string_missing(s)))
+        string_score_pairs.sort(key = lambda x: -x[1])
+        for n in range(num_to_output):
+            print(string_score_pairs[n][0], round(string_score_pairs[n][1], 1))
+
+
 
     
